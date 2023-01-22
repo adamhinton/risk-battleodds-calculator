25a94fa795cd4f9c654bf966262745ea853d607a
Adam Hinton <ahinton@myhealthily.com>
2023-01-22 12:35:02 -0500

    don't need numSimulations yet, deleted it

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { ResourceLimits } from 'worker_threads';
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +10,7 @@ function App() {
 
 export default App;
 
+// TYPE DEFINITIONS
 type PlayerCounts = {
   attackerCount: number;
   defenderCount: number;
@@ -20,25 +23,24 @@ type Results = {
 
 type UserInputs = {
   playerCounts: PlayerCounts;
-  numSimulations: number;
+  // numSimulations: number;
 }
 
+// USEFUL VARIABLES
 const userInputs: UserInputs = {
   playerCounts: {
    attackerCount: 15,
    defenderCount: 10
   },
-  numSimulations: 20
+  // numSimulations: 20
 }
 
-
-const results = {
+const results: Results = {
   attackerOccupies: 0,
   defenderHolds: 0
 }
 
-
-
+// UTILS
 const randomIntFromInterval = (min: number, max: number) =>{ // min and max included 
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
