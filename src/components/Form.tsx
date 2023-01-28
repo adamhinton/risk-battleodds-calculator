83236539ba67df79c2fa8d3b938d62e4e5c28705
adamhinton <adam.d.hinton@gmail.com>
2023-01-28 15:11:57 -0500

    basic formValues types in Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,28 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 
 type Props = {};
 
+type PlayerCounts = {
+  attackerCount: number;
+  defenderCount: number;
+};
+
+type FormValues = {
+  playerCounts: PlayerCounts;
+  numSimulations: number;
+};
+
 const Form = (props: Props): ReactElement => {
+  const initialFormValues: FormValues = {
+    playerCounts: {
+      attackerCount: 10,
+      defenderCount: 10,
+    },
+    numSimulations: 10,
+  };
+
+  const [formValues, setFormValues] = useState<FormValues>();
+
   return (
     <form>
       <label htmlFor="attackers">Attackers:</label>
