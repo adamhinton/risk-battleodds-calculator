4f21215df1afa08708b2b6f51684ad79d1e90fbc
adamhinton <adam.d.hinton@gmail.com>
2023-01-29 13:13:24 -0500

    make skeleton Results.tsx

---
+++ src/components/Results.tsx
@@ -0,0 +1,11 @@
+// PLANNING
+// attacker wins %
+// defender wins %
+// average troops left
+// calculate results here? Or elsewhere?
+// if calculate results here, then take in formValues. Then this component would be "smart" and do the calculations
+// if calculate results elsewhere, take in Results. THen this component would be "dumb" and just display results
+
+const Results = () => {};
+
+export default Results;
