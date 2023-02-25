8b04927c9c9b5a0ea5a41c950926503d49806207
adamhinton <adam.d.hinton@gmail.com>
2023-02-24 22:56:03 -0500

    skeleton ResultsDisplay component

--- src/components/ResultsDisplay.tsx
+++ src/components/ResultsDisplay.tsx
@@ -1,11 +1,11 @@
-// PLANNING
-// attacker wins %
-// defender wins %
-// average troops left
-// calculate results here? Or elsewhere?
-// if calculate results here, then take in formValues. Then this component would be "smart" and do the calculations
-// if calculate results elsewhere, take in Results. THen this component would be "dumb" and just display results
+import { Results } from "../utils/resultsCalculator";
 
-const Results = () => {};
+type ResultsDisplayProps = {
+  results: Results;
+};
 
-export default Results;
+const ResultsDisplay = () => {
+  return <div></div>;
+};
+
+export default ResultsDisplay;
