2dd7ef9e15c130367d688238a9c642a20ebf7c2d
adamhinton <adam.d.hinton@gmail.com>
2023-03-01 23:59:13 -0500

    update snapshots

--- src/Jest tests/ResultsDisplay.test.tsx
+++ src/Jest tests/ResultsDisplay.test.tsx
@@ -6,8 +6,8 @@ import { Results } from "../utils/resultsCalculator";
 const fakeResults: Results = {
   attackerOccupies: 100,
   defenderHolds: 120,
-  averageAttackersLeft: 9,
-  averageDefendersLeft: 20,
+  averageAttackersLeft: 9.758,
+  averageDefendersLeft: 20.382,
 };
 
 test("[1] Renders without errors", () => {
@@ -19,6 +19,7 @@ test("[2] Displays text of results as expected", () => {
 
   const h2 = screen.getByTestId("results-h2");
   const attackerOccupies = screen.getByTestId("results-attacker-occupies");
+  const avgAttackersLeft = 1;
   const defenderHolds = screen.getByTestId("results-defenderholds");
 
   expect(h2).toHaveTextContent("Results");
