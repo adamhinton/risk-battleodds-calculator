28979f841d01ce1371d7894edd27abf3ea72554c
adamhinton <adam.d.hinton@gmail.com>
2023-03-01 22:34:26 -0500

    fakeResults object in results test

--- src/Jest tests/ResultsDisplay.test.tsx
+++ src/Jest tests/ResultsDisplay.test.tsx
@@ -1,3 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import renderer from "react-test-renderer";
 import ResultsDisplay from "../components/ResultsDisplay";
+import { Results } from "../utils/resultsCalculator";
+
+const fakeResults: Results = {
+  attackerOccupies: 100,
+  defenderHolds: 120,
+  averageAttackersLeft: 9,
+  averageDefendersLeft: 20,
+};
+
+test("[1] Renders without errors", () => {
+  render(<ResultsDisplay results={fakeResults} />);
+});
