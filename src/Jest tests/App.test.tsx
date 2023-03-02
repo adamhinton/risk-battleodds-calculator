be6e0936c7be780d778b81e17893fbd57c67650f
adamhinton <adam.d.hinton@gmail.com>
2023-03-02 00:14:34 -0500

    Add Sanity Check to App.test

--- src/Jest tests/App.test.tsx
+++ src/Jest tests/App.test.tsx
@@ -2,11 +2,15 @@ import { render } from "@testing-library/react";
 import App from "../App";
 import renderer from "react-test-renderer";
 
-test("[1] renders without errors", () => {
+test("[1] Sanity check", () => {
+  expect(1 + 1).toEqual(2);
+});
+
+test("[2] renders without errors", () => {
   render(<App />);
 });
 
-test("[2] Matches snapshot from 1.29.2023", () => {
+test("[3] Matches snapshot from 1.29.2023", () => {
   const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
 });
