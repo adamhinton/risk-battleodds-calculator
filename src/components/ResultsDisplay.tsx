895bc7d99b7f99c5a0496126fcb60ca7cb99dbc3
adamhinton <adam.d.hinton@gmail.com>
2023-03-01 23:57:09 -0500

    better testidlocations

--- src/components/ResultsDisplay.tsx
+++ src/components/ResultsDisplay.tsx
@@ -17,14 +17,18 @@ const ResultsDisplay = (props: ResultsDisplayProps) => {
   return (
     <section>
       <h2 data-testid="results-h2">Results</h2>
-      <div data-testid="results-attacker-occupies">
-        <p>Attacker Occupies: {attackerWinPercent * 100}%</p>
+      <div>
+        <p data-testid="results-attacker-occupies">
+          Attacker Occupies: {attackerWinPercent * 100}%
+        </p>
         <p>
           Average Attackers Left: {results.averageAttackersLeft?.toFixed(1)}
         </p>
       </div>
-      <div data-testid="results-defenderholds">
-        <p>Defender Holds: {defenderWinPercent * 100}%</p>
+      <div>
+        <p data-testid="results-defenderholds">
+          Defender Holds: {defenderWinPercent * 100}%
+        </p>
         <p>
           Average Defenders Left: {results.averageDefendersLeft?.toFixed(1)}
         </p>
