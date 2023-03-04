571bf59fd02b658b27b4c857ae4c726b53720b42
adamhinton <adam.d.hinton@gmail.com>
2023-03-04 13:49:47 -0500

    basic newGR() typing

--- src/utils/newGenerateResults.ts
+++ src/utils/newGenerateResults.ts
@@ -1,5 +1,31 @@
-const newGenerateResults = () => {
-  return "";
+export type UserInputs = {
+  attackerCount: number;
+  defenderCount: number[];
+  numSimulations: number;
+};
+
+type PlayerType = "attacker" | "defender";
+
+type AttackerRolls = [number, number?, number?];
+type DefenderRolls = [number, number?];
+type PlayerRolls = AttackerRolls | DefenderRolls;
+
+export type Results = {
+  attackerOccupies: number;
+  defenderHolds: number;
+  averageAttackersLeft?: number | null;
+  averageDefendersLeft?: number | null;
+};
+
+const newGenerateResults = (userInputs: UserInputs): Results => {
+  const results: Results = {
+    attackerOccupies: 0,
+    defenderHolds: 0,
+    averageAttackersLeft: null,
+    averageDefendersLeft: null,
+  };
+
+  return results;
 };
 
 export default newGenerateResults;
