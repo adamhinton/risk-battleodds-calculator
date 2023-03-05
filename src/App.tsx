fcc5c7d126cbb8dbedb3ad0ee4298e046c6bd631
adamhinton <adam.d.hinton@gmail.com>
2023-03-05 15:17:16 -0500

    unneeded imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 // import Form from "./components/Form";
 import ResultsDisplay from "./components/ResultsDisplay";
 import newGenerateResults from "./utils/newGenerateResults";
-import generateResults, { Results } from "./utils/resultsCalculator";
+import { Results } from "./utils/resultsCalculator";
 import Form from "./components/Form";
 
 // PLAN
