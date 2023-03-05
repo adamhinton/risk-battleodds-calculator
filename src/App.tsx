81fc6d1645ba6757ffe03cb80f853ab9118c31f6
adamhinton <adam.d.hinton@gmail.com>
2023-03-05 14:52:43 -0500

    code cleanup in App

--- src/App.tsx
+++ src/App.tsx
@@ -36,37 +36,4 @@ console.log(
   })
 );
 
-// for (let i = 0; i < 10000; i++) {
-//   const attackerRolls = generatePlayerRolls("attacker", 3);
-//   const defenderRolls = generatePlayerRolls("defender", 2);
-//   // console.log("attackerRolls:", attackerRolls);
-//   // console.log("defenderRolls:", defenderRolls);
-
-//   if (
-//     attackerRolls[0] > defenderRolls[0] &&
-//     attackerRolls[1]! > defenderRolls[1]!
-//   ) {
-//     console.count("attacker wins");
-//   } else if (
-//     defenderRolls[0] >= attackerRolls[0]! &&
-//     defenderRolls[1]! >= attackerRolls[1]!
-//   ) {
-//     console.count("defender wins");
-//   } else {
-//     console.count("tie");
-//   }
-// }
-
 export default App;
-
-// PLAN
-// take in userInputs:
-//attackerCount,
-// defenderCount: number[],
-// numSimulations
-
-// while attackerCount < 0 ,
-// loop through defenderCount.
-// for each: run single simulation with attackerCount and defenderCount. The original version of runSingleSimulation.
-// Will need to tweak rSS() a bit to make this happen
-// wrap rSS() in a broader function to help tabulate the results
