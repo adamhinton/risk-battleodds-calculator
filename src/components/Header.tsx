8d6e62130f2d2f39ddfcaf276b4d317dc39ec134
adamhinton <adam.d.hinton@gmail.com>
2023-03-08 20:58:28 -0500

    put AppBar and box in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,14 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+
+// import styled from "styled-components";
+
 const Header = () => {
-  return <header />;
+  return (
+    <AppBar>
+      <Box sx={{ flexGrow: 1 }}></Box>
+    </AppBar>
+  );
 };
 
 export default Header;
