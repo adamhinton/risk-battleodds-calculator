a56cba3b9ddbddf5a0405332b95f2dabee1d2243
adamhinton <adam.d.hinton@gmail.com>
2023-03-12 13:14:17 -0400

    skeleton useDarkMode hook

---
+++ hooks/useDarkMode.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const useDarkMode = () => {
+  const key = "isDarkMode";
+  const [isDark, setIsDark] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : undefined;
+    } catch (error) {
+      return false;
+    }
+  });
+
+  useEffect(() => {
+    //This adds or removes .dark class to both body and each individual pokemon cards
+    if (isDark) {
+      // logic
+    } else {
+      // more logic
+    }
+    try {
+      window.localStorage.setItem(key, isDark);
+    } catch (e) {
+      console.error("Error in setting preference");
+    }
+  });
+  return [isDark, setIsDark];
+};
+
+export default useDarkMode;
