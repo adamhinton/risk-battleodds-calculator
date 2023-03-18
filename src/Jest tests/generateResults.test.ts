cd4a18810e211f5277a5be6879731ddc77a68088
Adam Hinton <adam.d.hinton@gmail.com>
2023-03-18 12:56:33 -0400

    test typo

--- src/Jest tests/generateResults.test.ts
+++ src/Jest tests/generateResults.test.ts
@@ -5,7 +5,7 @@ describe("[1] sortPlayerRolls", () => {
 	test("[1] sortPlayerRolls sorts arrays of numbers (lengths 3, 2, or 1) in descending order", () => {
 		const rollsThree: AttackerRolls = [3, 5, 1];
 		const rollsTwo: AttackerRolls = [1, 5];
-		const rollsOne: AttackerRolls = [1, 5];
+		const rollsOne: AttackerRolls = [1];
 
 		const sortedRollsThree = sortPlayerRolls(rollsThree);
 		const sortedRollsTwo = sortPlayerRolls(rollsTwo);
@@ -13,6 +13,6 @@ describe("[1] sortPlayerRolls", () => {
 
 		expect(sortedRollsThree).toEqual([5, 3, 1]);
 		expect(sortedRollsTwo).toEqual([5, 1]);
-		expect(sortedRollsOne).toEqual([5, 1]);
+		expect(sortedRollsOne).toEqual([1]);
 	});
 });
