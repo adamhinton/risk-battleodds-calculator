b86044922c28af9a86dca846c6640362fdbf5314
Adam Hinton <adam.d.hinton@gmail.com>
2023-03-19 00:40:54 -0400

    axe unneeded imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useState } from "react";
+import { useState } from "react";
 // import Form from "./components/Form";
 import ResultsDisplay from "./components/ResultsDisplay";
 import { Results } from "./utils/resultsCalculator";
